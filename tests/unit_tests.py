d346aa621050fa324899c113769998e223aa098b
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-16 12:33:24 +0800

    Create unit_tests.py

---
+++ tests/unit_tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    proofchecker
+    ~~~~~
+    :copyright: (c) 2014-2015 by Halfmoon Labs, Inc.
+    :copyright: (c) 2016 blockstack.org
+    :license: MIT, see LICENSE for more details.
+"""
