8e5656f1a4ab3ad6b870c3c0b3419aa51c9e54ca
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-16 12:33:51 +0800

    Update unit_tests.py

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -7,3 +7,26 @@
     :copyright: (c) 2016 blockstack.org
     :license: MIT, see LICENSE for more details.
 """
+
+import os
+import sys
+import json
+import unittest
+import requests
+
+# Hack around absolute paths
+current_dir = os.path.abspath(os.path.dirname(__file__))
+parent_dir = os.path.abspath(current_dir + "/../")
+sys.path.insert(0, parent_dir)
+
+from blockstack_proofs import profile_to_proofs, profile_v3_to_proofs
+from blockstack_proofs import contains_valid_proof_statement
+from blockstack_proofs import get_proof_from_txt_record
+
+test_users = ['ryan', 'werner', 'muneeb', 'fredwilson']
+
+test_domains = [{"username": "muneeb", 'domain': 'muneebali.com'}]
+
+check_proofs = ['twitter', 'facebook', 'github']
+
+BASE_URL = 'https://resolver.onename.com/v2/users/'
