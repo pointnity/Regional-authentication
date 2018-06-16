4602dc2887fda6df433f10a4ed4d0dd7576ae478
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-16 12:26:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='blockstack-proofs',
+    name='Pointnity Network',
     version='0.0.10',
     url='https://github.com/blockstack/blockstack-proofs',
     license='MIT',
